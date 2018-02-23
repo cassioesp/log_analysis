5129f2a3741e1684982a9743c653108772507ccc
vagrant <vagrant@vagrant.vm>
2018-02-23 17:15:32 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+### Log Analysis
+
+### How to run
+1. Clone this project with:
+`git clone https://github.com/cassioesp/log_analysis.git`
+
+2. Run with Python:
+`python log_analysis.py`
+
+3. Be sure to have Python, psycopg2 library and PostGreSQL installed!
---
+++ log_analysis.py
@@ -0,0 +1,38 @@
+import psycopg2
+
+
+def main():
+    DB_NAME = "news"
+
+    conn = psycopg2.connect(database=DB_NAME)
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT title, COUNT(*) as views FROM articles, log WHERE articles.slug = substring(log.path, '[^/]*$') GROUP BY 	articles.title ORDER BY views DESC")
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+            print row[0] + '-', row[1], ' views'
+
+    print '\n'
+
+    cursor.execute("SELECT name, COUNT(*) as views FROM authors, articles, log WHERE authors.id = articles.author AND articles.slug = substring(log.path, '[^/]*$') GROUP BY authors.name ORDER BY views DESC")
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print row[0] + '-', row[1], ' views'
+
+    print '\n'
+
+    cursor.execute("SELECT SUBSTRING(to_char(log.time, 'DD/MM/YYYY'),0,11) AS day, COUNT(status)*100/CAST((SELECT COUNT(*) FROM log) AS Float) AS percentage FROM log WHERE status = '404 NOT FOUND' GROUP BY day HAVING COUNT(status)*100/CAST((SELECT COUNT(*) FROM log) AS Float) > 1;")
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print row[0] + '-', row[1], ' views'
+
+if __name__ == "__main__":
+    main()
+
